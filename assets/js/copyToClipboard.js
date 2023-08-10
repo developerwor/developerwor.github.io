33b3bb0544150df65ebff0f2ba81d11d6668e096
Myeongheon Han <122499446+developerwor@users.noreply.github.com>
2023-08-10 17:35:19 +0900

    Create copyToClipboard.js

---
+++ assets/js/copyToClipboard.js
@@ -0,0 +1,21 @@
+function copyToClipboard(elementId) {
+    // 텍스트를 복사할 요소를 가져옵니다.
+    var textElement = document.getElementById(elementId);
+
+    // 임시 input 요소를 생성합니다.
+    var tempInput = document.createElement('input');
+    tempInput.setAttribute('value', textElement.innerText);
+    
+    // 문서에 임시 input 요소를 추가합니다.
+    document.body.appendChild(tempInput);
+    
+    // 임시 input 요소의 내용을 선택하고 복사합니다.
+    tempInput.select();
+    document.execCommand('copy');
+    
+    // 임시 input 요소와 선택을 해제합니다.
+    document.body.removeChild(tempInput);
+    window.getSelection().removeAllRanges();
+
+    alert('복사되었습니다: ' + textElement.innerText);
+}
