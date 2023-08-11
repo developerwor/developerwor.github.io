250244d6d4ad09224f5ae62c6558627f8e112553
Myeongheon Han <122499446+developerwor@users.noreply.github.com>
2023-08-11 09:22:07 +0900

    Update copyToClipboard.js

--- assets/js/copyToClipboard.js
+++ assets/js/copyToClipboard.js
@@ -17,5 +17,23 @@ function copyToClipboard(elementId) {
     document.body.removeChild(tempInput);
     window.getSelection().removeAllRanges();
 
-    alert('이메일이 복사되었습니다');
+    // alert('이메일이 복사되었습니다');
+
+    var successMessage = document.createElement('div');
+    successMessage.innerText = '이메일이 복사되었습니다';
+    successMessage.style.position = 'fixed';
+    successMessage.style.top = '50%';
+    successMessage.style.left = '50%';
+    successMessage.style.transform = 'translate(-50%, -50%)';
+    successMessage.style.padding = '1em';
+    successMessage.style.background = 'white';
+    successMessage.style.border = '1px solid #ccc';
+    successMessage.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+    document.body.appendChild(successMessage);
+
+    // 일정 시간 후에 메시지를 제거합니다.
+    setTimeout(function () {
+        document.body.removeChild(successMessage);
+    }, 2000); // 2초 후에 메시지를 제거합니다.
 }
+
